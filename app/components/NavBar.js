7caf808b7b93ac815d79ea6ca6d2ea355651390e
Warren Phen <warren.phen@gmail.com>
2019-05-07 06:22:16 -0400

    fixed navbar overflow

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -30,89 +30,24 @@ export default function NavBar({headerTitle, onPressLeft, onPressRight, iconLeft
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F9F9F9',
-  },
-  button: {
-    height: 40,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: '#00C871',
-    borderWidth: 1,
-    borderRadius: 8,
-    marginVertical: 10,
-    padding: 10
-  },
-  contentContainer: {
-    flexGrow: 1,
-  },
   navContainer: {
-    height: HEADER_HEIGHT,
-    // marginHorizontal: 10,
+    height: HEADER_HEIGHT + 4,
   },
   statusBar: {
     height: STATUS_BAR_HEIGHT,
     backgroundColor: 'transparent',
   },
-  boldText: {
-    fontWeight: 'bold'
-  },
-  foodItem: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginHorizontal: 10,
-    marginVertical: 10
-  },
   bar: {
     flex: 1,
     flexDirection: 'row',
-    marginVertical: 5,
   },
   navBar: {
     height: NAV_BAR_HEIGHT,
     justifyContent: 'space-between',
     alignItems: 'center',
     flexDirection: 'row',
-    backgroundColor: 'transparent',
     borderBottomColor: "#BDBDBD",
-    // borderBottomWidth: 1
   },
-  titleStyle: {
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: 18,
-  },
-  box: {
-    marginHorizontal: 15,
-    backgroundColor: '#fff',
-    padding: 10,
-    borderRadius: 8,
-    marginBottom: 10,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 12,
-    },
-    shadowOpacity: 0.1,
-    shadowRadius: 15.00,
-    elevation: 24,
-  },
-  macrosText: {
-    color: 'white',
-    fontWeight: 'bold'
-  },
-  input: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    height: 50,
-    borderColor: '#BDBDBD',
-    borderWidth: 1,
-    borderRadius: 8,
-    marginVertical: 10,
-    padding: 10
-  }
 });
 
 
