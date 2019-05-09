2cd1a09aa9b4160b8a68c266a8a0b403d147cec9
Warren Phen <warren.phen@gmail.com>
2019-05-09 03:49:33 -0400

    fixed import

--- app/Navigation.js
+++ app/Navigation.js
@@ -31,9 +31,9 @@ import FavouritesView from './containers/main/FavouritesView';
 
 const OnboardingNavigator = createStackNavigator(
   {
-    // IdentificationView: { screen: IdentificationView },
-    // MeasurementView: { screen: MeasurementView },
-    // ActivityLevelView: { screen: ActivityLevelView },
+    IdentificationView: { screen: IdentificationView },
+    MeasurementView: { screen: MeasurementView },
+    ActivityLevelView: { screen: ActivityLevelView },
     MainGoalView: { screen: MainGoalView },
     WeeklyActivityView: { screen: WeeklyActivityView },
     GoalWeightView: { screen: GoalWeightView },
@@ -118,7 +118,7 @@ const TabNavigator = createBottomTabNavigator(
 );
 
 const AppNavigator = createSwitchNavigator({
-  Onboarding: OnboardingNavigator,
+  // Onboarding: OnboardingNavigator,
   Main: TabNavigator,
 });
 
--- app/components/SelectBox.js
+++ app/components/SelectBox.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { colors } from '../../constants';
+import { colors } from '../constants';
 
 export default function SelectBox({title, subTitle, onPress, isSelected}) {
   const color = isSelected ? colors.primary : null;
